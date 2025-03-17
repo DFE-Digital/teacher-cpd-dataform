c266f413ca12f53b3f2eb0e379f1b2d6b6a45b23
Emma Harvey <emma.harvey@education.gov.uk>
2025-03-17 09:25:35 +0000

    Adding additional line to clarify what the field is.

--- definitions/dfe_analytics_dataform.js
+++ definitions/dfe_analytics_dataform.js
@@ -221,7 +221,7 @@ dfeAnalyticsDataform({
             }, {
                 keyName: "mentor_profile_id",
                 dataType: "string",
-                description: "Regarding how the addition of a mentor triggers the creation of a new induction record: If the mentor is selected when adding an ECT to the service, then the induction record will be created with the mentor on it. If the mentor is changed/selected later the a new induction record is created"
+                description: "The participant_profile_id of an ECT's mentor. This field is always NULL for mentors. With regards to how the addition of a mentor triggers the creation of a new induction record: If the mentor is selected when adding an ECT to the service, then the induction record will be created with the mentor on it. If the mentor is changed/selected later the a new induction record is created"
             }, {
                 keyName: "participant_profile_id",
                 dataType: "string",
