87f5ad8f62d41a7b8735e8748922a9d0ff4ef88d
steven2.legg@education.gov.uk <steven2.legg@education.gov.uk>
2025-10-10 11:36:32 +0000

    Add headcount tables

---
+++ includes/headcounts.js
@@ -0,0 +1,18 @@
+/* To be updated by a Programme Delivery Manager once per quarter. */
+
+const headcounts = [
+   {
+   date: "2025-02-01",
+   ecteHeadcount: 23,
+   npqHeadcount: 15
+   },
+   {
+   date: "2025-10-10",
+   ecteHeadcount: 21,
+   npqHeadcount: 14
+   }
+];
+
+module.exports = {
+    headcounts
+};
