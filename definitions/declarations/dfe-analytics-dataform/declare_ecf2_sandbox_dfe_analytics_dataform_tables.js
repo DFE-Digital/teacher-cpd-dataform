29f6daf315303259135d9e2ad49d4db3bd794259
theo.phillips@education.gov.uk <theo.phillips@education.gov.uk>
2026-03-24 14:34:58 +0000

    Declaring sandbox tables

--- definitions/declarations/dfe-analytics-dataform/declare_ecf2_sandbox_dfe_analytics_dataform_tables.js
+++ definitions/declarations/dfe-analytics-dataform/declare_ecf2_sandbox_dfe_analytics_dataform_tables.js
@@ -1,5 +1,28 @@
 const tableNames = [
-    "persist_api_request"
+    "appropriate_bodies_latest",
+    "events",
+    "induction_periods_field_updates",
+    "induction_periods_latest",
+    "induction_periods_version",
+    "session_details",
+    "teachers_latest",
+    "induction_extensions_latest",
+    "gias_schools_latest",
+    "schools_latest",
+    "training_periods_latest",
+    "school_partnerships_latest",
+    "mentorship_periods_latest_latest",
+    "mentor_at_school_periods_latest",
+    "lead_providers_latest",
+    "ect_at_school_periods_latest",
+    "delivery_partners_latest",
+    "delcarations_latest",
+    "contract_periods_latest",
+    "active_lead_providers_latest",
+    "statements_latest",
+    "lead_provider_delivery_partnerships_latest",
+    "schedules_latest",
+    "users_latest"
 ];
 
 tableNames.forEach(tableName => {
